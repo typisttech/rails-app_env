f18835803cd549aa3c2b1870d5dfe39b71aefaac
TangRufus <tangrufus@gmail.com>
2025-05-27 21:44:10 +0100

    make `standardrb` happy

--- lib/rails/app_env/helpers.rb
+++ lib/rails/app_env/helpers.rb
@@ -2,7 +2,7 @@ module Rails
   module AppEnv
     module Helpers
       def app_env
-        return Rails.env unless ENV["APP_ENV"].present?
+        return Rails.env if ENV["APP_ENV"].blank?
 
         @_app_env ||= EnvironmentInquirer.new(ENV["APP_ENV"])
       end
