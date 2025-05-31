fc4594d0e83a66c763dca0e065f4f767eafe8dfd
TangRufus <tangrufus@gmail.com>
2025-05-31 18:22:26 +0100

    Test: Explicit specific environment variable when asserting with runner; Refactor tests

--- test/features/credentials_test.rb
+++ test/features/credentials_test.rb
@@ -1,137 +1,144 @@
 require_relative "../test_helper"
 require_relative "runner_helper"
+require_relative "file_helper"
 
 module Rails::AppEnv::FeaturesTest
   class CredentialsTest < ActiveSupport::TestCase
     include RunnerHelper
+    include FileHelper
 
-    CREDENTIALS_PATH_TEST_CASES = [
-      {
-        name: "is {APP_ENV}.yml.enc when both APP_ENV and RAILS_ENV are present",
+    CREDENTIALS_PATH_TEST_CASES = {
+      "is {APP_ENV}.yml.enc when both APP_ENV and RAILS_ENV are present": {
         expected_path: "config/credentials/foo.yml.enc",
-        app_env: "foo",
-        rails_env: "bar",
-        secret_key_base_dummy: "1",
-        touch_file: "config/credentials/foo.yml.enc"
+        touch_file: "config/credentials/foo.yml.enc",
+        env: {
+          "APP_ENV" => "foo",
+          "RAILS_ENV" => "bar"
+        }
       },
-      {
-        name: "falls back to credentials.yml.enc when {APP_ENV}.yml.enc is absent and both APP_ENV and RAILS_ENV are present",
+      "falls back to credentials.yml.enc when {APP_ENV}.yml.enc is absent and both APP_ENV and RAILS_ENV are present": {
         expected_path: "config/credentials.yml.enc",
-        app_env: "foo",
-        rails_env: "bar",
-        secret_key_base_dummy: "1"
+        env: {
+          "APP_ENV" => "foo",
+          "RAILS_ENV" => "bar"
+        }
       },
-      {
-        name: "is {APP_ENV}.yml.enc when APP_ENV is present but RAILS_ENV is blank",
+      "is {APP_ENV}.yml.enc when APP_ENV is present but RAILS_ENV is blank": {
         expected_path: "config/credentials/foo.yml.enc",
-        app_env: "foo",
-        touch_file: "config/credentials/foo.yml.enc"
+        touch_file: "config/credentials/foo.yml.enc",
+        env: {
+          "APP_ENV" => "foo"
+        }
       },
-      {
-        name: "falls back to credentials.yml.enc when {APP_ENV}.yml.enc is absent and APP_ENV is present but RAILS_ENV is blank",
+      "falls back to credentials.yml.enc when {APP_ENV}.yml.enc is absent and APP_ENV is present but RAILS_ENV is blank": {
         expected_path: "config/credentials.yml.enc",
-        app_env: "foo",
-        secret_key_base_dummy: "1"
+        env: {
+          "APP_ENV" => "foo"
+        }
       },
-      {
-        name: "is {RAILS_ENV}.yml.enc when APP_ENV is blank but RAILS_ENV is present",
+      "is {RAILS_ENV}.yml.enc when APP_ENV is blank but RAILS_ENV is present": {
         expected_path: "config/credentials/foo.yml.enc",
-        rails_env: "foo",
-        secret_key_base_dummy: "1",
-        touch_file: "config/credentials/foo.yml.enc"
+        touch_file: "config/credentials/foo.yml.enc",
+        env: {
+          "RAILS_ENV" => "foo"
+        }
       },
-      {
-        name: "falls back to credentials.yml.enc when {RAILS_ENV}.yml.enc is absent and when APP_ENV is blank but RAILS_ENV is present",
+      "falls back to credentials.yml.enc when {RAILS_ENV}.yml.enc is absent and when APP_ENV is blank but RAILS_ENV is present": {
         expected_path: "config/credentials.yml.enc",
-        rails_env: "foo",
-        secret_key_base_dummy: "1"
+        env: {
+          "RAILS_ENV" => "foo"
+        }
       },
-      {
-        name: "is {DEFAULT_RAILS_ENV}.yml.enc when both APP_ENV and RAILS_ENV are blank",
+      "is {DEFAULT_RAILS_ENV}.yml.enc when both APP_ENV and RAILS_ENV are blank": {
         expected_path: "config/credentials/#{DEFAULT_RAILS_ENV}.yml.enc",
         touch_file: "config/credentials/#{DEFAULT_RAILS_ENV}.yml.enc"
       },
-      {
-        name: "falls back to credentials.yml.enc when {DEFAULT_RAILS_ENV}.yml.enc is absent and both APP_ENV and RAILS_ENV are blank",
+      "falls back to credentials.yml.enc when {DEFAULT_RAILS_ENV}.yml.enc is absent and both APP_ENV and RAILS_ENV are blank": {
         expected_path: "config/credentials.yml.enc"
       }
-    ]
+    }
 
-    KEY_PATH_TEST_CASES = [
-      {
-        name: "is {APP_ENV}.key when both APP_ENV and RAILS_ENV are present",
+    KEY_PATH_TEST_CASES = {
+      "is {APP_ENV}.key when both APP_ENV and RAILS_ENV are present": {
         expected_path: "config/credentials/foo.key",
-        app_env: "foo",
-        rails_env: "bar",
-        secret_key_base_dummy: "1",
-        touch_file: "config/credentials/foo.key"
-      },
-      {
-        name: "falls back to master.key when {APP_ENV}.key is absent and both APP_ENV and RAILS_ENV are present",
-        expected_path: "config/master.key",
-        app_env: "foo",
-        rails_env: "bar",
-        secret_key_base_dummy: "1"
-      },
-      {
-        name: "is {APP_ENV}.key when APP_ENV is present but RAILS_ENV is blank",
-        expected_path: "config/credentials/foo.key",
-        app_env: "foo",
-        touch_file: "config/credentials/foo.key"
-      },
-      {
-        name: "falls back to master.key when {APP_ENV}.key is absent and APP_ENV is present but RAILS_ENV is blank",
-        expected_path: "config/master.key",
-        app_env: "foo"
-      },
-      {
-        name: "is {RAILS_ENV}.key when APP_ENV is blank but RAILS_ENV is present",
-        expected_path: "config/credentials/foo.key",
-        rails_env: "foo",
         touch_file: "config/credentials/foo.key",
-        secret_key_base_dummy: "1"
+        env: {
+          "APP_ENV" => "foo",
+          "RAILS_ENV" => "bar"
+        }
       },
-      {
-        name: "falls back to master.key when {RAILS_ENV}.key is absent and when APP_ENV is blank but RAILS_ENV is present",
+      "falls back to master.key when {APP_ENV}.key is absent and both APP_ENV and RAILS_ENV are present": {
         expected_path: "config/master.key",
-        rails_env: "foo",
-        secret_key_base_dummy: "1"
+        env: {
+          "APP_ENV" => "foo",
+          "RAILS_ENV" => "bar"
+        }
       },
-      {
-        name: "is {DEFAULT_RAILS_ENV}.key when both APP_ENV and RAILS_ENV are blank",
+      "is {APP_ENV}.key when APP_ENV is present but RAILS_ENV is blank": {
+        expected_path: "config/credentials/foo.key",
+        touch_file: "config/credentials/foo.key",
+        env: {
+          "APP_ENV" => "foo"
+        }
+      },
+      "falls back to master.key when {APP_ENV}.key is absent and APP_ENV is present but RAILS_ENV is blank": {
+        expected_path: "config/master.key",
+        env: {
+          "APP_ENV" => "foo"
+        }
+      },
+      "is {RAILS_ENV}.key when APP_ENV is blank but RAILS_ENV is present": {
+        expected_path: "config/credentials/foo.key",
+        touch_file: "config/credentials/foo.key",
+        env: {
+          "RAILS_ENV" => "foo"
+        }
+      },
+      "falls back to master.key when {RAILS_ENV}.key is absent and when APP_ENV is blank but RAILS_ENV is present": {
+        expected_path: "config/master.key",
+        env: {
+          "RAILS_ENV" => "foo"
+        }
+      },
+      "is {DEFAULT_RAILS_ENV}.key when both APP_ENV and RAILS_ENV are blank": {
         expected_path: "config/credentials/#{DEFAULT_RAILS_ENV}.key",
         touch_file: "config/credentials/#{DEFAULT_RAILS_ENV}.key"
       },
-      {
-        name: "falls back to master.key when {DEFAULT_RAILS_ENV}.key is absent and both APP_ENV and RAILS_ENV are blank",
+      "falls back to master.key when {DEFAULT_RAILS_ENV}.key is absent and both APP_ENV and RAILS_ENV are blank": {
         expected_path: "config/master.key"
       }
-    ]
+    }
 
-    CREDENTIALS_PATH_TEST_CASES.each_with_index do |args, i|
+    CREDENTIALS_PATH_TEST_CASES.each_key do |name|
       class_eval <<~RUBY, __FILE__, __LINE__ + 1
-        test "content path #{args[:name]}" do
-          assert_credentials_path(**CREDENTIALS_PATH_TEST_CASES[#{i}])
+        test "content path  #{name}" do
+          assert_credentials_path(**CREDENTIALS_PATH_TEST_CASES[:"#{name}"])
         end
       RUBY
     end
 
-    KEY_PATH_TEST_CASES.each_with_index do |args, i|
+    KEY_PATH_TEST_CASES.each_key do |name|
       class_eval <<~RUBY, __FILE__, __LINE__ + 1
-        test "key path #{args[:name]}" do
-          assert_key_path(**KEY_PATH_TEST_CASES[#{i}])
+        test "key path  #{name}" do
+          assert_key_path(**KEY_PATH_TEST_CASES[:"#{name}"])
         end
       RUBY
     end
 
     private
 
-    def assert_credentials_path(expected_path:, **options)
-      assert_runner Rails.root.join(expected_path), "Rails.configuration.credentials.content_path", **options
+    def assert_credentials_path(expected_path:, touch_file: nil, **options)
+      assert_runner_puts_with_file Rails.root.join(expected_path), "Rails.configuration.credentials.content_path", touch_file, **options
     end
 
-    def assert_key_path(expected_path:, **options)
-      assert_runner Rails.root.join(expected_path), "Rails.configuration.credentials.key_path", **options
+    def assert_key_path(expected_path:, touch_file: nil, **options)
+      assert_runner_puts_with_file Rails.root.join(expected_path), "Rails.configuration.credentials.key_path", touch_file, **options
+    end
+
+    def assert_runner_puts_with_file(expected, subject, file_path, **options)
+      with_file(file_path) do
+        assert_runner_puts expected, subject, **options
+      end
     end
   end
 end
