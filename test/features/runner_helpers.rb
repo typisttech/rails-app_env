81346f320501ca3b94de8778319b75cd4781b861
TangRufus <tangrufus@gmail.com>
2025-06-01 01:46:30 +0100

    Test: Remove `SECRET_KEY_BASE_DUMMY`

--- test/features/runner_helpers.rb
+++ test/features/runner_helpers.rb
@@ -2,7 +2,7 @@ module RunnerHelpers
   private
 
   def assert_runner_puts(expected, subject, env: {})
-    env = env.with_defaults({"APP_ENV" => nil, "RAILS_ENV" => nil, "SECRET_KEY_BASE_DUMMY" => "1"})
+    env = env.with_defaults({"APP_ENV" => nil, "RAILS_ENV" => nil})
 
     stdout, status = Open3.capture2(env, "#{DUMMY_ROOT}/bin/rails", "runner", "puts #{subject}")
 
